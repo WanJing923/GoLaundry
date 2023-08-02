d6df8aaa955ee1ec773191388a9a7bab572a27cc
Wan <p20012339@student.newinti.edu.my>
2023-08-03 01:44:57 +0800

    Sign up and home layout

---
+++ app/src/main/java/com/example/golaundry/HomeFragment.java
@@ -0,0 +1,128 @@
+package com.example.golaundry;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    private Toolbar toolbar;
+    private LineChart lineChart;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        toolbar = (Toolbar) view.findViewById(R.id.fh_toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+        setHasOptionsMenu(true);
+
+//        BarChart barChart = view.findViewById(R.id.chart1);
+//        ArrayList<BarEntry> barEntries = new ArrayList<>();
+//        barEntries.add(new BarEntry(0f, 44f));
+//        barEntries.add(new BarEntry(1f, 88f));
+//        barEntries.add(new BarEntry(2f, 41f));
+//        barEntries.add(new BarEntry(3f, 85f));
+//        barEntries.add(new BarEntry(4f, 96f));
+//        barEntries.add(new BarEntry(5f, 25f));
+//        barEntries.add(new BarEntry(6f, 10f));
+//        BarDataSet barDataSet = new BarDataSet(barEntries, "Dates");
+//        ArrayList<String> theDates = new ArrayList<>();
+//        theDates.add("Mars");
+//        theDates.add("Avril");
+//        theDates.add("Dec");
+//        theDates.add("May");
+//        theDates.add("OCt");
+//        theDates.add("Nov");
+//        theDates.add("Fir");
+//        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(theDates));
+//        BarData theData = new BarData(barDataSet);//----Line of error
+//        barChart.setData(theData);
+//        barChart.setTouchEnabled(false);
+//        barChart.setDragEnabled(false);
+//        barChart.setScaleEnabled(false);
+
+        return view;
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // First clear current all the menu items
+        menu.clear();
+
+        // Add the new menu items
+        inflater.inflate(R.menu.menu_top, menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.tm_btn_notification) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
