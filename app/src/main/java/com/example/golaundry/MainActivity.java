215b2bc113aca1d31a37fcc0f2ea67883ab49dff
Wan <p20012339@student.newinti.edu.my>
2023-08-05 01:29:51 +0800

    some changes and connect vp login register

--- app/src/main/java/com/example/golaundry/MainActivity.java
+++ app/src/main/java/com/example/golaundry/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.golaundry;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 import android.os.Bundle;
+
+import com.example.golaundry.adapter.MainAdapter;
+import com.example.golaundry.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //implement binding method
+        com.example.golaundry.databinding.ActivityMainBinding mActivityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mActivityMainBinding.getRoot());
+
+        mActivityMainBinding.mainTab.setupWithViewPager(mActivityMainBinding.mainVp);
+
+        MainAdapter vpAdapter = new MainAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addfragment(new LoginFragment(), "LOGIN");
+        vpAdapter.addfragment(new SignUpFragment(), "SIGN UP");
+        mActivityMainBinding.mainVp.setAdapter(vpAdapter);
+
     }
 }
