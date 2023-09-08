2e35de138fbb99df25cac4f226daf43539f85942
WanJing <lauwan08@gmail.com>
2023-09-09 04:01:10 +0800

    combine and save opening hrs

--- app/src/main/java/com/example/golaundry/LaundryEditInfoActivity.java
+++ app/src/main/java/com/example/golaundry/LaundryEditInfoActivity.java
@@ -2,16 +2,10 @@ package com.example.golaundry;
 
 import android.annotation.SuppressLint;
 import android.app.TimePickerDialog;
-import android.content.DialogInterface;
-import android.content.res.Resources;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.NumberPicker;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,10 +15,11 @@ import com.example.golaundry.viewModel.LaundryViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
-
 
 public class LaundryEditInfoActivity extends AppCompatActivity {
 
@@ -37,10 +32,10 @@ public class LaundryEditInfoActivity extends AppCompatActivity {
     private Button timeFriday;
     private Button timeSaturday;
     private Button timeSunday;
+    List<String> allTimeRanges; // List to store all time ranges
 
     private final Calendar[] startTimes = new Calendar[7];
     private final Calendar[] endTimes = new Calendar[7];
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +45,7 @@ public class LaundryEditInfoActivity extends AppCompatActivity {
 
         String currentUserId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
 
-        //set each time
+        // Set each time range for 7 days
         for (int i = 0; i < 7; i++) {
             startTimes[i] = Calendar.getInstance();
             endTimes[i] = Calendar.getInstance();
@@ -64,7 +59,7 @@ public class LaundryEditInfoActivity extends AppCompatActivity {
         timeSaturday = findViewById(R.id.alei_btn_saturday_time);
         timeSunday = findViewById(R.id.alei_btn_sunday_time);
 
-        //for each button pressed
+        // Set click listeners for each button
         timeMonday.setOnClickListener(v -> showTimePickerDialog(0));
         timeTuesday.setOnClickListener(v -> showTimePickerDialog(1));
         timeWednesday.setOnClickListener(v -> showTimePickerDialog(2));
@@ -73,9 +68,19 @@ public class LaundryEditInfoActivity extends AppCompatActivity {
         timeSaturday.setOnClickListener(v -> showTimePickerDialog(5));
         timeSunday.setOnClickListener(v -> showTimePickerDialog(6));
 
+        ImageView doneImageView = findViewById(R.id.alei_iv_done);
+        doneImageView.setOnClickListener(v -> updateInfo());
     }
 
-    //start time picker
+    private void updateInfo() {
+        if (allTimeRanges == null) {
+            Toast.makeText(this, "Please set the opening hours", Toast.LENGTH_SHORT).show();
+        } else {
+//insert to db
+        }
+    }
+
+    // start time picker dialog
     private void showTimePickerDialog(int dayIndex) {
 
         Calendar startTime = startTimes[dayIndex];
@@ -94,7 +99,7 @@ public class LaundryEditInfoActivity extends AppCompatActivity {
         timePickerDialog.show();
     }
 
-    //end time picker
+    //end time picker dialog
     private void showEndTimePickerDialog(int dayIndex) {
 
         Calendar endTime = endTimes[dayIndex];
@@ -113,7 +118,7 @@ public class LaundryEditInfoActivity extends AppCompatActivity {
         timePickerDialog.show();
     }
 
-    //update button text
+    //update button text, add all inside list
     @SuppressLint("SetTextI18n")
     private void updateTimeRangeButton(int dayIndex) {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
@@ -124,8 +129,17 @@ public class LaundryEditInfoActivity extends AppCompatActivity {
         long differenceInMillis = end.getTimeInMillis() - start.getTimeInMillis();
         boolean isLessThanOneHour = differenceInMillis < 3600000;
 
-        String startTimeText = sdf.format(start.getTime());
-        String endTimeText = sdf.format(end.getTime());
+        String startTimeText;
+        String endTimeText;
+
+        if (isLessThanOneHour) {
+            startTimeText = "00:00";
+            endTimeText = "00:00";
+            Toast.makeText(this, "Opening hour should not be less than one hour", Toast.LENGTH_SHORT).show();
+        } else {
+            startTimeText = sdf.format(start.getTime());
+            endTimeText = sdf.format(end.getTime());
+        }
 
         String time = startTimeText + " - " + endTimeText;
 
@@ -153,34 +167,17 @@ public class LaundryEditInfoActivity extends AppCompatActivity {
                 break;
         }
 
-        if (isLessThanOneHour) {
-            switch (dayIndex) {
-                case 0:
-                    timeMonday.setText("00:00 - 00:00");
-                    break;
-                case 1:
-                    timeTuesday.setText("00:00 - 00:00");
-                    break;
-                case 2:
-                    timeWednesday.setText("00:00 - 00:00");
-                    break;
-                case 3:
-                    timeThursday.setText("00:00 - 00:00");
-                    break;
-                case 4:
-                    timeFriday.setText("00:00 - 00:00");
-                    break;
-                case 5:
-                    timeSaturday.setText("00:00 - 00:00");
-                    break;
-                case 6:
-                    timeSunday.setText("00:00 - 00:00");
-                    break;
-            }
-            Toast.makeText(LaundryEditInfoActivity.this, "Opening hour should not be less than one hour", Toast.LENGTH_SHORT).show();
+        //create a new list
+        if (allTimeRanges == null) {
+            allTimeRanges = new ArrayList<>();
+        } else {
+            allTimeRanges.clear();
+        }
+
+        //add into list for all days
+        for (int i = 0; i < 7; i++) {
+            String timeRange = sdf.format(startTimes[i].getTime()) + " - " + sdf.format(endTimes[i].getTime());
+            allTimeRanges.add(timeRange);
         }
     }
-
-
-
 }
