ca6d79112156a32460a7d4657dfbe36778e6ac46
WanJing <lauwan08@gmail.com>
2023-10-05 10:59:56 +0800

    history-initialize

--- app/src/main/java/com/example/golaundry/HistoryFragment.java
+++ app/src/main/java/com/example/golaundry/HistoryFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -13,13 +15,11 @@ import com.google.android.material.tabs.TabLayout;
 public class HistoryFragment extends Fragment {
 
     public HistoryFragment() {
-        // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -27,7 +27,28 @@ public class HistoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_history, container, false);
-//        TabLayout historyTabLayout = view.findViewById(R.id.history_tab_fragment);
+        TabLayout historyUserTab = view.findViewById(R.id.hf_tab);
+        RecyclerView OrderUserRecyclerView = view.findViewById(R.id.hf_rv_orders);
+
+//        List<YourDataModel> initialData = getInitialData();
+//        YourRecyclerViewAdapter adapter = new YourRecyclerViewAdapter(initialData);
+//        recyclerView.setAdapter(adapter);
+
+        historyUserTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Update RecyclerView data based on the selected tab
+//                String selectedTabText = tab.getText().toString();
+//                List<YourDataModel> newData = getNewDataForTab(selectedTabText);
+//                adapter.updateData(newData);
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/example/golaundry/HistoryRiderFragment.java
+++ app/src/main/java/com/example/golaundry/HistoryRiderFragment.java
@@ -3,62 +3,52 @@ package com.example.golaundry;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HistoryRiderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.tabs.TabLayout;
+
 public class HistoryRiderFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public HistoryRiderFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HistoryRiderFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HistoryRiderFragment newInstance(String param1, String param2) {
-        HistoryRiderFragment fragment = new HistoryRiderFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history_rider, container, false);
+        View view =  inflater.inflate(R.layout.fragment_history_rider, container, false);
+
+        TabLayout historyRiderTab = view.findViewById(R.id.hrf_tab);
+        RecyclerView OrderRiderRecyclerView = view.findViewById(R.id.hrf_rv_orders);
+
+//        List<YourDataModel> initialData = getInitialData();
+//        YourRecyclerViewAdapter adapter = new YourRecyclerViewAdapter(initialData);
+//        recyclerView.setAdapter(adapter);
+
+        historyRiderTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Update RecyclerView data based on the selected tab
+//                String selectedTabText = tab.getText().toString();
+//                List<YourDataModel> newData = getNewDataForTab(selectedTabText);
+//                adapter.updateData(newData);
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+        return view;
     }
 }
