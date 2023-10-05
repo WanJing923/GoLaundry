d0a611756dfbbddfa0616cf85942504fe6c0ac7f
WanJing <lauwan08@gmail.com>
2023-10-06 01:16:09 +0800

    history-user fragment generate qr code

--- app/src/main/java/com/example/golaundry/adapter/HistoryFragmentAdapter.java
+++ app/src/main/java/com/example/golaundry/adapter/HistoryFragmentAdapter.java
@@ -1,17 +1,35 @@
 package com.example.golaundry.adapter;
 
+import static android.content.Context.WINDOW_SERVICE;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.zxing.WriterException;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,6 +37,7 @@ import com.example.golaundry.R;
 import com.example.golaundry.model.OrderModel;
 import com.example.golaundry.model.ServiceItem;
 import com.example.golaundry.viewModel.LaundryViewModel;
+import com.google.zxing.WriterException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -28,10 +47,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
 public class HistoryFragmentAdapter extends RecyclerView.Adapter<HistoryFragmentAdapter.ViewHolder> {
     private final List<OrderModel> orderList;
     private final Context context;
     private final LaundryViewModel mLaundryViewModel;
+
+    QRGEncoder qrgEncoder;
+    Bitmap bitmap;
+    String TAG = "GenerateQRCODE";
 
     public HistoryFragmentAdapter(List<OrderModel> orderList, Context context, LaundryViewModel mLaundryViewModel) {
         this.orderList = orderList;
@@ -110,11 +136,59 @@ public class HistoryFragmentAdapter extends RecyclerView.Adapter<HistoryFragment
         }
 
         mLaundryViewModel.getServiceData(order.getLaundryId()).observe((LifecycleOwner) context, service -> {
-            if (service!=null){
+            if (service != null) {
                 HistoryFragmentServicesAdapter mHistoryFragmentServicesAdapter = new HistoryFragmentServicesAdapter(servicesList, service, context);
                 holder.servicesRecyclerView.setAdapter(mHistoryFragmentServicesAdapter);
                 holder.servicesRecyclerView.setLayoutManager(new LinearLayoutManager(context));
             }
+        });
+
+        holder.qrImageView.setOnClickListener(view -> {
+            String orderId = order.getOrderId();
+
+
+            WindowManager manager = (WindowManager) context.getSystemService(WINDOW_SERVICE);
+            Display display = manager.getDefaultDisplay();
+            Point point = new Point();
+            display.getSize(point);
+            int width = point.x;
+            int height = point.y;
+            int smallerDimension = width < height ? width : height;
+            smallerDimension = smallerDimension * 3 / 4;
+
+            qrgEncoder = new QRGEncoder(orderId, null, QRGContents.Type.TEXT, smallerDimension);
+
+            try {
+                bitmap = qrgEncoder.encodeAsBitmap();
+                Dialog qrCodeDialog = new Dialog(context);
+                qrCodeDialog.setContentView(R.layout.dialog_qr_code);
+                ImageView qrCodeImageView = qrCodeDialog.findViewById(R.id.qr_show);
+                qrCodeImageView.setImageBitmap(bitmap);
+                qrCodeDialog.show();
+
+            } catch (WriterException e) {
+                Log.v(TAG, e.toString());
+            }
+
+//            QRGEncoder qrgEncoder = new QRGEncoder(orderId, null, QRGContents.Type.TEXT, 400);
+//
+//            try {
+//                // Generate the QR code as a byte array.
+//                byte[] qrCodeByteArray = qrgEncoder.encodeAsBytes();
+//
+//                // Convert the byte array to a bitmap.
+//                Bitmap qrCodeBitmap = BitmapFactory.decodeByteArray(qrCodeByteArray, 0, qrCodeByteArray.length);
+//
+//                // Create a dialog to display the QR code.
+//                Dialog qrCodeDialog = new Dialog(context);
+//                qrCodeDialog.setContentView(R.layout.dialog_qr_code);
+//                ImageView qrCodeImageView = qrCodeDialog.findViewById(R.id.qr_show);
+//                qrCodeImageView.setImageBitmap(qrCodeBitmap);
+//                qrCodeDialog.show();
+//            } catch (WriterException e) {
+//                e.printStackTrace();
+//            }
+
         });
     }
 
@@ -143,7 +217,7 @@ public class HistoryFragmentAdapter extends RecyclerView.Adapter<HistoryFragment
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView laundryShopNameTextView, orderIdTextView, statusTextView, deliveryAmountTextView, totalAmountTextView, dateTextView, currentStatusTextView;
         RecyclerView servicesRecyclerView;
-        ImageView moreImageView;
+        ImageView moreImageView, qrImageView;
         Button actionButton;
 
         public ViewHolder(View itemView) {
@@ -158,6 +232,7 @@ public class HistoryFragmentAdapter extends RecyclerView.Adapter<HistoryFragment
             dateTextView = itemView.findViewById(R.id.huli_tv_date);
             currentStatusTextView = itemView.findViewById(R.id.huli_tv_status);
             actionButton = itemView.findViewById(R.id.huli_button);
+            qrImageView = itemView.findViewById(R.id.huli_iv_qr);
         }
     }
 }
